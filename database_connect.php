296f538bc634f118a5582485927ca3e92035f673
jeremynathan <jeremy.nathan1@gmail.com>
2019-06-18 15:40:36 +0800

    umbz

--- database_connect.php
+++ database_connect.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $connection = mysqli_connect('localhost', 'root', '');
 if (!$connection){
@@ -8,14 +7,4 @@ $select_db = mysqli_select_db($connection, 'alzheimer');
 if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection));
 }
-=======
-<?php
-$connection = mysqli_connect('localhost', 'root', '');
-if (!$connection){
-    die("Database Connection Failed" . mysqli_error($connection));
-}
-$select_db = mysqli_select_db($connection, 'alzheimer');
-if (!$select_db){
-    die("Database Selection Failed" . mysqli_error($connection));
-}
->>>>>>> 91cd998ea2b7ee0c34873b678aeb1877a220e175
+?>
